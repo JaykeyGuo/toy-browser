b0a1cc870238a375c19a281a02b4c5e3002fe4e9
Jaykey.Guo <jkland927@gmail.com>
2020-05-13 23:08:12 +0800

    first lession

--- client.js
+++ client.js
@@ -17,12 +17,11 @@ class Request {
     } else if (this.headers['Content-Type'].includes('application/x-www-form-urlencoded')) {
       this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
     }
-    // console.log(this.bodyText, '0000')
     this.headers['Content-Length'] = this.bodyText.length;
   };
 
   toString() {
-    return `${this.method}${this.path} HTTP/1.1\r
+    return `${this.method} ${this.path} HTTP/1.1\r
 ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}
 \r
 ${this.bodyText}`;
@@ -30,10 +29,10 @@ ${this.bodyText}`;
 
   send(connection) {
     return new Promise((resolve, reject) => {
+      const parser = new ResponseParser();
       if (connection) {
         connection.write(this.toString());
       } else {
-        // console.log(this,'090909');
         connection = net.createConnection({
           host: this.host,
           port: this.port,
@@ -41,6 +40,17 @@ ${this.bodyText}`;
           connection.write(this.toString());
         })
       }
+      connection.on('data', (data) => {
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+        }
+        connection.end();
+      });
+      connection.on('error', (err) => {
+        reject(err);
+        connection.end();
+      });
     })
   }
 }
@@ -49,58 +59,157 @@ class Response {
 
 }
 
-let request = new Request({
-  method: 'POST',
-  host: '127.0.0.1',
-  port: '8088',
-  path: '/',
-  headers: {
-    ['Content-Type']: 'application/x-www-form-urlencoded',
-    ['X-Foo2']: 'customed'
-  },
-  body: {
-    name: 'jaykey'
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = '';
   }
-})
-// console.log(request);
 
-request.send();
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
 
-// const client = net.createConnection({
-//   host: '127.0.0.1',
-//   port: 8088
-// }, () => {
-//   // 'connect' listener.
-//   console.log('connected to server!');
-//   let request = new Request({
-//     method: 'POST',
-//     host: '127.0.0.1',
-//     port: '8088',
-//     path: '/',
-//     headers: {
-//       ['Content-Type']: 'application/x-www-form-urlencoded',
-//       ['X-Foo2']: 'customed'
-//     },
-//     body: {
-//       name: 'jaykey'
-//     }
-//   })
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
 
-//   console.log(request.toString());
-//   client.write(request.toString());
-//   // client.write('Host: 127.0.0.1\r\n');
-//   // client.write('Content-Type: application/x-www-form-urlencoded\r\n');
-//   // client.write('\r\n');
-//   // client.write('field1=aaa&code=x%3D1\r\n');
-// });
-// client.on('data', (data) => {
-//   console.log(data.toString());
-//   client.end();
-// });
-// client.on('end', () => {
-//   console.log('disconnected from server');
-// });
-// // client.on('error', (err) => {
-// //   console.log(err);
-// //   client.end();
-// // });
+  receive(string) {
+    for(let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      } else {
+        this.statusLine += (char);
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WAITING_HEADER_BLOCK_END;
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TruckedBodyParser();
+        }
+      } else {
+        this.headerName += (char);
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = '';
+        this.headerValue = '';
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_BODY;
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      this.bodyParser.receiveChar(char);
+    }
+  }
+}
+class TruckedBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITING_NEW_LINE = 3;
+    this.WAITING_NEW_LINE_END = 4;
+
+    this.length = 0;
+    this.content = [];
+    this.isFinished = false;
+    this.current = this.WAITING_LENGTH;
+  }
+
+  receiveChar(char) {
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === '\r') {
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+        this.current = this.WAITING_LENGTH_LINE_END;
+      } else {
+        this.length *= 10;
+        this.length += char.charCodeAt(0) - '0'.charCodeAt(0);
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      console.log(this.content)
+      this.length --;
+      if (this.length === 0) {
+        this.current = this.WAITING_NEW_LINE;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_LENGTH;
+      }
+    }
+  }
+}
+
+void async function() {
+  let request = new Request({
+    method: 'GET',
+    host: '127.0.0.1',
+    port: '8088',
+    path: '/',
+    headers: {
+      ['Content-Type']: 'application/x-www-form-urlencoded',
+      ['X-Foo2']: 'customed'
+    },
+    body: {
+      name: 'jaykey'
+    }
+  })
+  let response = await request.send();
+  console.log(response);
+}();
--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.writeHead('Connection', 'keep-alive');
+  // res.writeHead('Connection', 'keep-alive');
+  // res.write('hello\n');
   res.end('ok');
 })
 
 server.listen(8088);
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   console.log('receive request')
+//   console.log(req.headers);
+//   res.write('hello\n');
+//   res.end();
+// })
+
+// server.listen(8088)
